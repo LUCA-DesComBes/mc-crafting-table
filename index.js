ac3fd908a96590f4485b8dc1370422c79044685b
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2025-01-27 15:52:28 +0100

    feat (app) : first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const MATERIAL_IRON_INGOT_ID = "IRON-INGOT";
+const MATERIAL_STICK_ID = "STICK";
+const MATERIAL_AIR_ID = "";
+
+const MATERIAL_AIR_IMG = "./assets/air.webp";
+const MATERIAL_IRON_INGOT_IMG = "./assets/iron-ingot.webp";
+const MATERIAL_STICK_IMG = "./assets/stick.webp";
+
+const materials = [MATERIAL_AIR_ID, MATERIAL_IRON_INGOT_ID, MATERIAL_STICK_ID];
+const materialsImageSrc = [
+	MATERIAL_AIR_IMG,
+	MATERIAL_IRON_INGOT_IMG,
+	MATERIAL_STICK_IMG,
+];
+
+const pickaxeRecipe = [
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_STICK_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_STICK_ID,
+	MATERIAL_AIR_ID,
+];
+const axeRecipe = [
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_IRON_INGOT_ID,
+	MATERIAL_STICK_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_AIR_ID,
+	MATERIAL_STICK_ID,
+	MATERIAL_AIR_ID,
+];
+
+const pickaxeRecipeImageSrc = "./assets/iron-pickaxe.webp";
+const axeRecipeImageSrc = "./assets/iron-axe.webp";
+
+const recipeList = [pickaxeRecipe, axeRecipe];
+const recipeImageSrcList = [pickaxeRecipeImageSrc, axeRecipeImageSrc];
+
+const craftingTable = ["", "", "", "", "", "", "", "", ""];
+const inventoryTable = [1, 2, 1];
+
+// Coder ici
+const inventoryRows = document.getElementById("inv-r");
+
+function createElement(tag, attributes = {}) {
+	const element = document.createElement(tag);
+	for (const key in attributes) {
+		if (key === "className") element.className = attributes[key];
+		else element.setAttribute(key, attributes[key]);
+	}
+	return element;
+}
+for (let i = 0; i < inventoryTable.length; i++) {
+	if (inventoryTable[i] == 1) {
+		const btnInv = createElement("button", { className: "case-inv" });
+		inventoryRows.appendChild(btnInv)
+		const ingotImg = createElement("")
+	}
+}
